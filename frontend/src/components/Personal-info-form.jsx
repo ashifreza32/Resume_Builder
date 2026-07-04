34e6c94ada13203f2aefe22ba39a372b99f7198c
Ashif Reza <ashiffazan@gmail.com>
2026-07-05 00:34:04 +0530

    feat: add PersonalInfoForm component for user data input

---
+++ frontend/src/components/Personal-info-form.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrersonalInfoForm = ({data, onChange, removeBg, setRemoveBg }) => {
+  return (
+    <div>
+        <h2>Personal Information</h2>
+        </div>
+  );
+};
+
+export default PrersonalInfoForm;
