e6558e0abc53f0215a88a147bcd28de89e8c913f
Ashif Reza <ashiffazan@gmail.com>
2026-07-06 03:07:56 +0530

    fix: correct spelling of PersonalInfoForm and update handleFieldChange function

--- frontend/src/components/Personal-info-form.jsx
+++ frontend/src/components/Personal-info-form.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const PrersonalInfoForm = ({data, onChange, removeBg, setRemoveBg }) => {
+
+    const handleFieldChange = (field,value) => {
+        onchange({
+            ...data,
+            [field]: value
+        }) 
+
+    }
   return (
     <div>
         <h2>Personal Information</h2>
